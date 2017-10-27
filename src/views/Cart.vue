642e722e877730d1d644b5c4ff243d06237f2f7d
Sharingan100 <1a1a1a.262626.0010@gmail.com>
2017-10-28 02:31:48 +0700

    finished dummy api request

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -16,45 +16,86 @@
       <mu-raised-button secondary label="Get Recommend items" fullWidth @click="handleRecommend()" />
     </mu-content-block>
     <mu-sub-header>Recommend List</mu-sub-header>
-    <mu-content-block>
-      {{recommendList}}
-    </mu-content-block>
+
+    <div v-if="errors.length != 0" style="color: red;">
+      <mu-content-block>
+        {{ errors.toString() }}
+      </mu-content-block>
+    </div>
+    <div v-else>
+      <mu-content-block>
+        {{recommendList}}
+      </mu-content-block>
+    </div>
+
     <mu-content-block>
       <mu-raised-button primary label="Checkout" fullWidth @click="handleCheckout()" />
     </mu-content-block>
   </div>
 </template>
 <script>
-import { mapState } from 'vuex'
+import { mapState } from "vuex";
+import axios from "axios";
+
 export default {
   computed: {
-    ...mapState(['user'])
+    ...mapState(["user"])
   },
   data() {
     return {
-      product: '',
-      shelfList: ['a', 'b'],
+      product: "",
+      shelfList: [29126, 7076, 12820],
       selectList: [],
-      recommendList: []
-    }
+      recommendList: [],
+      errors: []
+    };
   },
   methods: {
     handleSelect() {
-      var val = this.product
-      this.selectList.push(val)
+      var val = this.product;
+      this.selectList.push(val);
     },
     handleClear() {
-      this.selectList = []
+      this.selectList = [];
     },
     handleRecommend() {
       // connect to webservice & get recommend list
-      this.recommendList = []
+      this.recommendList = [];
     },
     handleCheckout() {
       // go to check out
+      // TODO: change post url to our recommender api
+      axios
+        .post(`http://jsonplaceholder.typicode.com/posts`, {
+          itemID: this.selectList
+        })
+        .then(response => {
+          console.log("====================response");
+          console.log(response);
+
+          // dummy data
+          response = {
+            result: "success",
+            recommend_itemId: "36361"
+          };
+          // TODO: response.result
+          if (response.result === "success") {
+
+            this.recommendList = response.recommend_itemId;
+
+          } else {
+            // handle unsuccessful result
+            console.log("====================response.result");
+            console.log(response.result);
+            this.errors.push("Sorry, Something went wrong. Please try again");
+          }
+        })
+        .catch(e => {
+          this.errors.push(e);
+        }); 
     }
   }
-}
+};
 </script>
 <style scoped>
 
